a8e47dcafc29828b6791fc417033918775ad6486
AgustínNicolásPalomeque <palomequeagus2006@gmail.com>
2025-08-20 15:13:54 -0300

    agregue musica

--- estoy_probando.py
+++ estoy_probando.py
@@ -2,7 +2,7 @@ import webbrowser
 
 def abrir_youtube():
     # URL de la canción "Mónaco" de Desakata2 en YouTube
-    url = "https://www.youtube.com/watch?v=OofkFIcvrGU&list=RDOofkFIcvrGU&start_radio=1"
+    url = "https://www.youtube.com/watch?v=VE-FlMVqFKE&list=RDVE-FlMVqFKE&start_radio=1"
     
     # Abrir la URL en el navegador predeterminado
     webbrowser.open(url)
