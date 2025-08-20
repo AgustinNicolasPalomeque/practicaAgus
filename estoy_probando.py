1938bb2f5ee28bf93e8fbdd38d03542e4303543b
AgustínNicolásPalomeque <palomequeagus2006@gmail.com>
2025-08-20 13:10:23 -0300

    agregue cosas al archivo

--- estoy_probando.py
+++ estoy_probando.py
@@ -1,1 +1,11 @@
-print("hoy es 20/08/2025")
+import webbrowser
+
+def abrir_youtube():
+    # URL de la canción "Mónaco" de Desakata2 en YouTube
+    url = "https://www.youtube.com/watch?v=OofkFIcvrGU&list=RDOofkFIcvrGU&start_radio=1"
+    
+    # Abrir la URL en el navegador predeterminado
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    abrir_youtube()
